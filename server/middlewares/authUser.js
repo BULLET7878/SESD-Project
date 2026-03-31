ca5814df927058e61d8de35c38ee02588f69745f
Rahul Dhakar <rahuldhakarmm@gmail.com>
2026-04-01 01:30:10 +0530

    feat(server): add JWT auth middlewares for user and seller

---
+++ server/middlewares/authUser.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+
+const authUser = async (req, res, next) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        return res.json({
+            success: false,
+            message: "Not Authorized"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded.id) {
+            return res.json({
+                success: false,
+                message: "Not Authorized"
+            });
+        }
+
+
+        req.userId = decoded.id;
+
+        next();
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+export default authUser;
